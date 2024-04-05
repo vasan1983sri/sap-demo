b1f397af78a53ddc08aba7692e9ca5989f4a7ca0
srinivasan shanmugam <vasan1983sri@gmail.com>
2024-04-05 07:54:56 -0400

    Added User Info Details

--- src/components/body/components/UserDetailsForm.js
+++ src/components/body/components/UserDetailsForm.js
@@ -4,7 +4,21 @@ export default function UserDetailsForm() {
   return (
     <div>
         <div>
-            <h1>Hello World - Display UserDetails Form</h1>
+            <div> 
+            <h1>User Details</h1>
+              <input type="text" id="first_Name" placeholder="First Name" style={{marginRight: "8px"}} />
+              <input type="text" id="middle_Name" placeholder="Middle Name" style={{marginRight: "8px"}} />
+              <input type="text" id="last_Name" placeholder="Last Name" style={{marginRight: "8px"}} />
+            </div>
+            <div>
+            <h1>Contact Info</h1>
+            <input type="text" placeholder="(123)-567-7890)" />
+              <input type="text" placeholder="example@email.com" />
+              <input type="text" placeholder="Address" />
+              <input type="text" placeholder="City" />
+              <input type="text" placeholder="State" />
+              <input type="text" placeholder="Zip Code" />
+            </div>
         </div>
     </div>
   )
